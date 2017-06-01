bdd834823cac741c9b442e418eba9f30cf136012
denyskrysko <denyskrysko@gmail.com>
2017-06-01 16:22:39 -0400

    Written query for Employee, Employer, Status and Review tables. (createDB.sql file)

--- createDB.sql
+++ createDB.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE Person(
-  id BIGINT(20) NOT NULL IDENTITY,
+  id BIGINT(20) NOT NULL IDENTITY PRIMARY KEY,
   firstName VARCHAR(100) NOT NULL,
   lastName VARCHAR(100) NOT NULL,
   dateOfBirth DATE,           <-- ???
@@ -14,4 +14,33 @@ CREATE TABLE Person(
   province VARCHAR(10) NOT NULL,
   profilePhoto IMAGE,
   rating INT(10)
+)
+
+CREATE TABLE Employee(
+  id BIGINT(20) NOT NULL IDENTITY PRIMARY KEY,
+  person_id BIGINT(20) NOT NULL
+  rating INT(10),                 <-- figure out
+    FOREIGN KEY (person_id)
+    REFERENCES Person (id)
+    ON DELETE CASCADE
+)
+
+CREATE TABLE Employer(
+  id BIGINT(20) NOT NULL IDENTITY PRIMARY KEY,
+  person_id BIGINT(20) NOT NULL
+  rating INT(10),                 <-- figure out
+    FOREIGN KEY (person_id)
+    REFERENCES Person (id)
+    ON DELETE CASCADE
+)
+
+CREATE TABLE STATUS(
+  id BIGINT(20) NOT NULL IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Review(
+  id BIGINT(20) NOT NULL IDENTITY PRIMARY KEY,
+  rating INT(10)                  <--figure out
+  description VARCHAR(250)
 )
